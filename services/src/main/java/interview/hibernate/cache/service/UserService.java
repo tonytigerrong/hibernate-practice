50cecf915198a7ca2afd57f8f424ec6716179f0f
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-07-19 01:42:34 -0300

    option 2 for ehcache 1. add another <cache> config in ehcache.xml 2. use query.setCacheable and setCacheRegiion

--- services/src/main/java/interview/hibernate/cache/service/UserService.java
+++ services/src/main/java/interview/hibernate/cache/service/UserService.java
@@ -4,6 +4,7 @@ import interview.hibernate.cache.dao.UserRepo;
 import interview.hibernate.cache.model.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -27,8 +29,23 @@ public class UserService {
      */
     @Cacheable(value="usersCache", key="#id")
     public User getUser(Long id){
-        logger.info("extract user={} from db",id); // only show 1 time if, for same id query
+        logger.info("getUser: extract user={} from db",id); // only show 1 time if, for same id query
         return userRepo.getUserById(id);
+    }
+
+    /**
+     * setCacheable(true) / setCacheRegion : same result with @cacheable 
+     * @param id
+     * @return
+     */
+    @Cacheable(value="usersCache2", key="#id")
+    public User getUser2(Long id){
+        logger.info("getUser2: extract user={} from db",id); // only show 1 time if, for same id query
+        Session session = entityManager.unwrap(Session.class);
+        Query query = session.createQuery("From User where id=:id").setParameter("id",id);
+        List<User> users = query.setCacheable(true).setCacheRegion("user").list();
+        if(users.size() == 1) return users.get(0);
+        else return null;
     }
     @Transactional
     public void addUser(User user){
