e45f92a70b5e41a0b35ddcc3a2224fbe47a1aa96
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-07-19 02:12:23 -0300

    add show cache content and size

--- web/src/main/java/interview/hibernate/cache/controller/UserController.java
+++ web/src/main/java/interview/hibernate/cache/controller/UserController.java
@@ -2,6 +2,7 @@ package interview.hibernate.cache.controller;
 
 import interview.hibernate.cache.model.User;
 import interview.hibernate.cache.service.UserService;
+import net.sf.ehcache.CacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,5 +21,17 @@ public class UserController {
     @GetMapping("/user2/{id}")
     public User getUserById2(@PathVariable("id") Long id){
         return userService.getUser2(id);
+    }
+
+    /**
+     * get Cache content by cache key 
+     * @return
+     */
+    @GetMapping("/user/getCache")
+    public Integer getUserCache(){
+        int size = CacheManager.ALL_CACHE_MANAGERS.get(0)
+                .getCache("usersCache").getSize();
+
+        return size;
     }
 }
